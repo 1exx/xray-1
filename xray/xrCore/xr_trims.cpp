7852ee2c8b7616c38f587a941f289f61824f5d78
1exx <gitlab@oneex.me>
2017-03-18 00:48:17 +0500

    Force Conformance in For Loop Scope enabled: xrCore, xrXMLParser

--- xray/xrCore/xr_trims.cpp
+++ xray/xrCore/xr_trims.cpp
@@ -4,9 +4,13 @@
 LPSTR _TrimLeft( LPSTR str )
 {
 	LPSTR p 	= str;
-	while( *p && (u8(*p)<=u8(' ')) ) p++;
-    if (p!=str){
-        for (LPSTR t=str; *p; t++,p++) *t=*p;
+	while( *p && (u8(*p) <= u8(' ')) )
+		p++;
+    if (p!=str)
+	{
+		LPSTR t = str;
+        for (; *p; t++,p++)
+			*t=*p;
         *t = 0;
     }
 	return str;
